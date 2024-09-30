203319c34483c48824f1736b9e283730be074ec1
DSeach <djseach@gmail.com>
2024-09-30 23:22:34 +0800

    Made it look less crap
    todo
    * make new animal page

--- FrontEnd Web Interface/ict302-animals-frontend/src/Components/Upload/Tagging.tsx
+++ FrontEnd Web Interface/ict302-animals-frontend/src/Components/Upload/Tagging.tsx
@@ -4,7 +4,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import { MenuItem, Select, FormControl, InputLabel, Typography, Box, SelectChangeEvent } from '@mui/material';
 
 interface TaggingProps {
   open: boolean;
@@ -12,37 +12,77 @@ interface TaggingProps {
 }
 
 //TODO 
-//Dont upload media until tagging is done
-//implement new animal page
-//make it look not crap
+//Don't upload media until tagging is done
+//Implement new animal page
+//Make it look not crap
 
 export default function Tagging({ open, handleClose }: TaggingProps) {
+  const [selectedAnimal, setSelectedAnimal] = React.useState('name1');
+  const [generator, setGenerator] = React.useState('GART');
+
+
+  const handleAnimalChange = (event: SelectChangeEvent<string>) => {
+    setSelectedAnimal(event.target.value);
+  };
+
+  const handleGeneratorChange = (event: SelectChangeEvent<string>) => {
+    setGenerator(event.target.value);
+  };
+
   return (
-    <Dialog open={open} onClose={handleClose}>
+    <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
       <DialogTitle>Upload Successful</DialogTitle>
       <DialogContent>
-        Your file has been uploaded successfully!
-        <br/>
-        Please enter animal details to continue.
-        <br/>
-        Animal name:
-        <select name="cars" id="cars">
-            <option value="name1">name1</option>
-            <option value="name2">name2</option>
-            <option value="name3">name3</option>
-            <option value="name4">name4</option>
-        </select>
-        <Button>go to new Animal page</Button> 
-        <br/>
-        Generator : 
-        <select name="Generators" id="Generators">
-            <option value="GART">GART</option>
-        </select>
+        {/* Informational text */}
+        <Typography variant="body1" gutterBottom>
+          Your file has been uploaded successfully! Please enter animal details to continue.
+        </Typography>
 
+        {/* Animal selection */}
+        <Box mt={2}>
+          <FormControl fullWidth margin="normal">
+            <InputLabel id="animal-select-label">Animal Name</InputLabel>
+            <Select
+              labelId="animal-select-label"
+              id="animals"
+              value={selectedAnimal}
+              onChange={handleAnimalChange}
+            >
+              <MenuItem value="Jax">Jax</MenuItem>
+              <MenuItem value="Max">Max</MenuItem>
+              <MenuItem value="Fernet">Fernet</MenuItem>
+              <MenuItem value="Aqua">Aqua</MenuItem>
+            </Select>
+          </FormControl>
+          <Button color="primary" variant="outlined" style={{ marginTop: '10px' }}>
+            Add new Animal
+          </Button>
+        </Box>
+
+        {/* Generator selection */}
+        <Box mt={3}>
+          <FormControl fullWidth margin="normal">
+            <InputLabel id="generator-select-label">Generator</InputLabel>
+            <Select
+              labelId="generator-select-label"
+              id="Generators"
+              value={generator}
+              onChange={handleGeneratorChange}
+            >
+              <MenuItem value="GART">GART</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
       </DialogContent>
+
       <DialogActions>
-        <Button onClick={handleClose} color ="primary">Generate</Button>
-        <Button onClick={handleClose} color="primary">Close</Button>
+        {/* Actions */}
+        <Button onClick={handleClose} color="primary" variant="contained">
+          Generate
+        </Button>
+        <Button onClick={handleClose} color="primary" variant="outlined">
+          Close
+        </Button>
       </DialogActions>
     </Dialog>
   );
